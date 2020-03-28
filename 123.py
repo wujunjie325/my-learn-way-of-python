5e6e48f609ca6e5e9f6379de735311aa43ecf872
wujunjie325 <51513192+wujunjie325@users.noreply.github.com>
2020-03-28 20:41:18 +0800

    sort

---
+++ 123.py
@@ -0,0 +1,35 @@
+import random
+arr=[1,3,4,1,6,2,8,9,0,5,3]
+for i in range(len(arr)):
+    arr[i]=random.randint(1,10)
+#冒泡排序
+def maopao_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+def select_sort(arr):
+    for i in range(len(arr)-1):
+        cul = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[cul]:
+                cul = j
+        arr[cul], arr[i] = arr[i], arr[cul]
+def insert_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                arr.insert(j,arr.pop(i))
+                break
+def insertsoet(arr):
+    for i in range(len(arr)):
+        for j in range(i-1,-1,-1):
+            if arr[i] > arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
+                break
+insertsoet(arr)
+#maopao_sort(arr)
+print(arr)
+#select_sort(arr)
+insert_sort(arr)
+print(arr)
